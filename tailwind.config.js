c449809338365e0fec4d764a13ad489287ad4b1e
heykirman <75514294+sukirman1901@users.noreply.github.com>
2024-05-01 13:14:07 +0700

    Nidejia Dashboard is a sleek and intuitive frontend interface designed to simplify your data visualization experience. Built with the Tailwind CSS framework, this dashboard offers a seamless user experience with striking aesthetics.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'dashboard-gray' : '#F5F6F8',
+        'orange-mood' : '#FF9357',
+      },
+    },
+  },
+  plugins: [],
+}
+
